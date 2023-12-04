968f326e6d653f9b45c6f891c6ae8a27246d6e8f
Szabolcs Bazil Papp <papp.szabolcs.bazil@gmail.com>
2023-12-04 17:30:28 +0100

    remove overgrown branching control flow

--- src/main/java/hu/aestallon/adventofcode/year2023/day01/Trebuchet.java
+++ src/main/java/hu/aestallon/adventofcode/year2023/day01/Trebuchet.java
@@ -23,9 +23,13 @@ import java.util.OptionalInt;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 
-import static hu.aestallon.adventofcode.year2023.util.Digits.digit;
-
 public final class Trebuchet {
+
+  private static final String[] NUMBERS = {
+      "one", "two", "three",
+      "four", "five", "six",
+      "seven", "eight", "nine"
+  };
 
   static Trebuchet create(List<String> lines) {
     return new Trebuchet(lines);
@@ -39,27 +43,12 @@ public final class Trebuchet {
   }
 
   private static OptionalInt match(String s) {
-    if (s.startsWith("1") || s.startsWith("one")) {
-      return OptionalInt.of(1);
-    } else if (s.startsWith("2") || s.startsWith("two")) {
-      return OptionalInt.of(2);
-    } else if (s.startsWith("3") || s.startsWith("three")) {
-      return OptionalInt.of(3);
-    } else if (s.startsWith("4") || s.startsWith("four")) {
-      return OptionalInt.of(4);
-    } else if (s.startsWith("5") || s.startsWith("five")) {
-      return OptionalInt.of(5);
-    } else if (s.startsWith("6") || s.startsWith("six")) {
-      return OptionalInt.of(6);
-    } else if (s.startsWith("7") || s.startsWith("seven")) {
-      return OptionalInt.of(7);
-    } else if (s.startsWith("8") || s.startsWith("eight")) {
-      return OptionalInt.of(8);
-    } else if (s.startsWith("9") || s.startsWith("nine")) {
-      return OptionalInt.of(9);
-    } else {
-      return OptionalInt.empty();
+    for (int i = 1; i <= NUMBERS.length; i++) {
+      if (s.startsWith(String.valueOf(i)) || s.startsWith(NUMBERS[i - 1])) {
+        return OptionalInt.of(i);
+      }
     }
+    return OptionalInt.empty();
   }
 
   private final List<String> lines;
